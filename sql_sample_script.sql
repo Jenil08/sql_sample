dd23b9942904c111c5741a927caeab8ccf483315
Jenil More <more jenil>
2025-01-30 16:41:32 +0530

    Added new table

--- sql_sample_script.sql
+++ sql_sample_script.sql
@@ -1,1 +1,16 @@
+DROP DATABASE sql_sample;
+
 CREATE DATABASE sql_sample;
+
+USE sql_sample;
+
+CREATE TABLE customer (
+	id INT,
+    custname VARCHAR(100)
+    );
+    
+    INSERT INTO customer (id, custname) VALUES
+    (1, 'ABC Co'),
+    (2, 'XYZ Corp),
+    (3, 'BN plumbing');
+    
